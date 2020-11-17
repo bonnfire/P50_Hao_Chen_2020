05de43c2bf59545fd03f28f879b0e6e8eed37727
Bonnie Lin <b2lin@ucsd.edu>
2020-11-17 13:51:33 -0800

    add cohorts 1-3 to db

---
+++ CREATE/CREATE_CSV.R
@@ -0,0 +1,5 @@
+# SAVE CSV
+# save file 
+write.csv(WFU_Chen_excel_orig_test_df, file = "~/Desktop/Database/csv files/p50_hao_chen_2020/chen_01_02_wfu_metadata.csv", row.names = F) 
+write.csv(chen_03_wfu_metadata, file = "~/Desktop/Database/csv files/p50_hao_chen_2020/chen_03_wfu_metadata.csv", row.names = F) 
+
